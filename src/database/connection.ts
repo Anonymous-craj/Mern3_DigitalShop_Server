e002c3c01e960662e196d5f3201ab0da8fa9d577
Anonymous-craj <sirajbasnet5@gmail.com>
2025-09-22 22:20:38 +0545

    setup backend server and connected databse through postgres

---
+++ src/database/connection.ts
@@ -0,0 +1,19 @@
+import { Sequelize } from "sequelize-typescript";
+import { envConfig } from "../config/config";
+
+const sequelize = new Sequelize(envConfig.connectionString as string);
+
+try {
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log("Database connection successful");
+    })
+    .catch((err) => {
+      console.log("Error aayo", err);
+    });
+} catch (error) {
+  console.log(error);
+}
+
+export default sequelize;
