e83e1eed60874585daf04bf3b242bfb11bd07dfd
Anonymous-craj <sirajbasnet5@gmail.com>
2025-09-30 22:45:34 +0545

    added role based middleware where only admin can access certain api's

--- src/middleware/userMiddleware.ts
+++ src/middleware/userMiddleware.ts
@@ -1,10 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { envConfig } from "../config/config";
+import User from "../database/models/userModel";
 
+export enum Role {
+  Admin = "admin",
+  Customer = "customer",
+}
+
+interface AuthRequest extends Request {
+  user?: {
+    username: string;
+    email: string;
+    password: string;
+    role: string;
+    id: string;
+  };
+}
 class UserMiddleware {
   static async isUserLoggedIn(
-    req: Request,
+    req: AuthRequest,
     res: Response,
     next: NextFunction
   ): Promise<void> {
@@ -18,16 +33,36 @@ class UserMiddleware {
     }
 
     // Validating if the token is legit or not?
-    jwt.verify(token, envConfig.jwtSecretKey, (err, result) => {
+    jwt.verify(token, envConfig.jwtSecretKey, async (err, result: any) => {
       if (err) {
         res.status(403).json({
           message: "Invalid token!!!",
         });
       } else {
-        console.log(result);
+        const userData = await User.findByPk(result.userId); //User data find garerw dinxa(email, password, role)
+        if (!userData) {
+          res.status(404).json({
+            message: "No user with that userId",
+          });
+          return;
+        }
+        req.user = userData;
         next();
       }
     });
+  }
+
+  static accessTo(...roles: Role[]) {
+    return (req: AuthRequest, res: Response, next: NextFunction) => {
+      let userRole = req.user?.role as Role;
+      if (!roles.includes(userRole)) {
+        res.status(403).json({
+          message: "You do not have permission to perform this action!",
+        });
+        return;
+      }
+      next();
+    };
   }
 }
 
--- src/routes/categoryRoute.ts
+++ src/routes/categoryRoute.ts
@@ -1,15 +1,23 @@
 import express, { Router } from "express";
 import CategoryController from "../controllers/categoryController";
-import UserMiddleware from "../middleware/userMiddleware";
+import UserMiddleware, { Role } from "../middleware/userMiddleware";
 const router: Router = express.Router();
 
 router
   .route("/")
   .get(CategoryController.fetchCategories)
-  .post(UserMiddleware.isUserLoggedIn, CategoryController.addCategory);
+  .post(
+    UserMiddleware.isUserLoggedIn,
+    UserMiddleware.accessTo(Role.Admin),
+    CategoryController.addCategory
+  );
 router
   .route("/:id")
   .patch(CategoryController.updateCategory)
-  .delete(CategoryController.deleteCategory);
+  .delete(
+    UserMiddleware.isUserLoggedIn,
+    UserMiddleware.accessTo(Role.Admin),
+    CategoryController.deleteCategory
+  );
 
 export default router;
