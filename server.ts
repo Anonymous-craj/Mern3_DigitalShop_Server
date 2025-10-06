a662df476df29289fa8d0b2d12ed104488e732f5
Anonymous-craj <sirajbasnet5@gmail.com>
2025-10-06 22:37:29 +0545

    integrated websocket for realtime orderStatus change

--- server.ts
+++ server.ts
@@ -2,13 +2,69 @@ import adminSeeder from "./adminSeeder";
 import app from "./src/app";
 import { envConfig } from "./src/config/config";
 import CategoryController from "./src/controllers/categoryController";
-
+import { Server } from "socket.io";
+import jwt from "jsonwebtoken";
+import User from "./src/database/models/userModel";
 function startServer() {
   const port = envConfig.port || 4000;
-  app.listen(port, () => {
+  const server = app.listen(port, () => {
     CategoryController.seedCategory();
     console.log(`Server has started at port[${port}]`);
     adminSeeder();
+  });
+
+  const io = new Server(server); //Passing argument http request in the Server class because the first request is always a http request before connecting in websocket(Three way handshake)
+
+  //Made an array onLineUsers to track users if they are online as soon as they connect with our project
+  let onlineUsers: { socketId: string; userId: string; role: string }[] = [];
+  let addToOnlineUsers = (socketId: string, userId: string, role: string) => {
+    onlineUsers = onlineUsers.filter((user) => user.userId !== userId);
+    onlineUsers.push({ socketId, userId, role });
+  };
+
+  //websocket connection
+  io.on("connection", (socket) => {
+    const token = socket.handshake.headers.token; //jwt
+
+    //Checking if the token provided is legit or not
+    if (token) {
+      console.log(token);
+
+      jwt.verify(
+        token as string,
+        envConfig.jwtSecretKey,
+        async (err, result: any) => {
+          if (err) {
+            socket.emit("error", err);
+          } else {
+            const userData = await User.findByPk(result.userId); //User data find garerw dinxa(email, password, role)
+            if (!userData) {
+              socket.emit("error", "No user with that id!");
+              return;
+            }
+            //If token is verified grab userId
+            addToOnlineUsers(socket.id, result.userId, userData.role);
+          }
+        }
+      );
+    } else {
+      socket.emit("error", "Please provide token!");
+    }
+
+    socket.on("updateOrderStatus", (data) => {
+      const { status, orderId, userId } = data;
+      //Check if the userId is in our onlineUsers Array
+      const findUser = onlineUsers.find((user) => user.userId); //(socketId, userId, role)
+      if (findUser) {
+        io.to(findUser.socketId).emit(
+          "success",
+          "OrderStatus Updated Successfully!"
+        );
+        console.log(findUser);
+      } else {
+        socket.emit("error", "User is not online!");
+      }
+    });
   });
 }
 
